44dd111496a6fe874793f0b47713f3aef25b0324
GustafKisi <gustafeden@gmail.com>
2021-12-05 22:48:30 +0100

    day 1 part 2

---
+++ day1/part2/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void shift(int *arr) {
+  arr[0] = arr[1]; arr[1] = arr[2]; arr[2] = 0;
+}
+
+int sum(int *arr) {
+  return arr[0] + arr[1] + arr[2];
+}
+
+int main()
+{
+  FILE *fp;
+  char buff[16];
+  int ints[3] = {0};
+  int lastSum, nextSum, count = 0;
+
+  fp = fopen("input.txt", "r");
+  // populate the first 3 values
+  for (size_t i = 0; i < 3; i++)
+  {
+    fgets(buff, 16, (FILE*)fp);
+    sscanf(buff, "%d", &ints[i]);
+  }
+  // set the first lastSum
+  lastSum = sum(ints);
+  // shift
+  shift(ints);
+  // run main loop
+  while (fgets(buff, 16, (FILE*)fp) != NULL)
+  {
+    // read into last array buffer
+    sscanf(buff, "%d", &ints[2]);
+    // set next sum
+    nextSum = sum(ints);
+    // if new sum is larger than last sum inc counter.
+    if (nextSum > lastSum) { count++; }
+    // set this sum as last sum
+    lastSum = nextSum;
+    // shift
+    shift(ints);
+  }
+  printf("count: %d", count);
+  fclose(fp);
+}
